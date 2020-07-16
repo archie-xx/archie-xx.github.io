1a035521080622e3df99995081efbe4b04fbcaa8
Timofei Ustiantsev <tuistssip@gmail.com>
2020-07-16 19:56:53 +0500

    Add files via upload

---
+++ precache-manifest.c6bf5327ba41b3752d310da164d26abb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf406d5cf7f2f5288aef",
+    "url": "/css/app.b40b5235.css"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "/fonts/Rubik-Regular.b3d0902b.ttf"
+  },
+  {
+    "revision": "ec42d7850060a10dacb9e04816853bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf406d5cf7f2f5288aef",
+    "url": "/js/app.6d977880.js"
+  },
+  {
+    "revision": "eca2dbbbda52a16b7d94",
+    "url": "/js/chunk-72637835.99cdb34c.js"
+  },
+  {
+    "revision": "8082a439690012a2b399",
+    "url": "/js/chunk-vendors.96ce1b8e.js"
+  },
+  {
+    "revision": "da62d018ffe3766a4a2c84b10f92f563",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
