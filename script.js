05fdc2919a586669c8e974cc2816301597d5bd10
tuist <tuistssip@gmail.com>
2020-08-04 15:13:18 +0500

    add infobox

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ word.addEventListener('click', () => {
   const video = document.querySelector('#back-video')
   video.muted = !video.muted
   word.classList.toggle('full-brightness')
+  document.querySelector('.wrapper').classList.toggle('darken')
+  document.querySelector('.blurred').classList.toggle('shown')
 })
