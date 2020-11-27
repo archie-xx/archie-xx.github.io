65c1a30dfab7b3b2ba1b63e0807c1d4ef5ccfbbf
Archie <tuistssip@gmail.com>
2020-11-28 00:38:07 +0500

    sd

--- README.md
+++
@@ -1,1 +0,0 @@
-# raritetmolodoy.github.io
--- script.js
+++
@@ -1,6 +0,0 @@
-const word = document.querySelector('.title')
-word.addEventListener('click', () => {
-  const video = document.querySelector('#back-video')
-  video.muted = !video.muted
-  word.classList.toggle('full-brightness')
-})
